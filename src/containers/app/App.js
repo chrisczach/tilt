93e711450679420f7b1664e4c677317b4069b901
Chris Czach <chrisczach@gmail.com>
2018-12-13 10:11:05 -0800

    setup

---
+++ src/containers/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import classes from './App.module.css'
+import gn from '../../utilities/gyro';
+
+class App extends Component {
+  render() {
+    return (
+      <>test</>
+    );
+  }
+}
+
+export default App;
