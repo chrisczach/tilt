885d71a8f63f07651e89683ab164ccc85eb9ddca
Christopher Czach <chrisczach@gmail.com>
2018-12-14 21:01:26 -0800

    test

--- src/components/dot/dot.js
+++ src/components/dot/dot.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import classes from './dot.module.css';
 const Dot = (props) => {
-  const x = Math.min(Math.max(props.x,-90),90);
-  const y = props.y;
-
+  const x = Math.round(Math.min(Math.max(props.x,-90),90));
+  const y = Math.round(props.y);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: `translate(calc(-50% + ${y}px), -50% + ${y}px)`
+  }
   return (
-    <div className={classes.dot}>{x} {y}</div>
+    <div style={style}>{x} {y}</div>
 )
 }
 
